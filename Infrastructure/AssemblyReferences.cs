38c129aa2e01858d7e818c66422b390e629c3ec1
Suman Rai <officeproj111@gmail.com>
2024-01-30 15:28:35 +0545

    partially implement message queue

--- Infrastructure/AssemblyReferences.cs
+++ Infrastructure/AssemblyReferences.cs
@@ -1,17 +1,22 @@
 ﻿using Application.Abstraction;
+using Application.Abstraction.EventBus;
+using Application.Authentication.Commands.Register.RegisteredUser;
 using Domain.Abstraction;
 using Domain.Entities;
 using Domain.IRepositories;
 using Infrastructure.Cache;
 using Infrastructure.Data;
 using Infrastructure.JWT;
+using Infrastructure.MessageBroker;
 using Infrastructure.Repositories;
 using Infrastructure.Seed;
+using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
@@ -30,6 +35,41 @@ namespace Infrastructure
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            #region Message Queue
+            //Configure apsetting.json value to MessageBrokerSetting class
+            services.Configure<MessageBrokerSettings>(
+                configuration.GetSection("MessageBroker"));
+
+            //Take the value from MEssageBroker Setting and inject to DependencyInjection
+            services.AddSingleton(sp =>
+            sp.GetRequiredService<IOptions<MessageBrokerSettings>>().Value) ;
+
+            //Message queue are used to transport message using busConfiguration
+            services.AddMassTransit(busConfigurator =>
+            {
+                //Add another configuration that provides configuration options for message bus for endpoint
+                busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+                busConfigurator.AddConsumer<RegisteredCreatedEventConsumer>();
+
+                busConfigurator.UsingRabbitMq((context, configurator) =>
+                {
+                    //Resolve message broke setting into variable
+                    MessageBrokerSettings setings = context.GetRequiredService<MessageBrokerSettings>();
+
+                    //Use this setting to connect to rabbitmq bus
+                    configurator.Host(new Uri(setings.Host), h =>
+                    {
+                        h.Username(setings.Username);
+                        h.Password(setings.Password);
+                    });
+
+
+                });
+            });
+
+            #endregion
 
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -62,6 +102,7 @@ namespace Infrastructure
             services.AddScoped<IVDCRepository, VDCRepository>();
             services.AddScoped<IProvinceRepository, ProvinceRepository>();
             services.AddScoped<IinitializeRepository, InitializeRepository>();
+            services.AddTransient<IEventBus, EventBus>();
 
             return services;
         }
